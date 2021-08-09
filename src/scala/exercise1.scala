dd9e0ac67ad43d4fa3cf8f135f2e6c8b35fab571
ritesh kumar <riteshkumar@riteshs-MacBook-Pro.local>
2021-08-09 18:14:07 +0530

    first Exercise completed

--- src/scala/exercise1.scala
+++ src/scala/exercise1.scala
@@ -1,6 +1,8 @@
 package scala
-import scala.concurrent.Future
+import scala.FuturesOptionsHandling.clicks
+import scala.concurrent.{Await, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
 
 
 
@@ -10,66 +12,33 @@ import scala.concurrent.ExecutionContext.Implicits.global
     val powersOfTwo = List[Int](8, 32, 2, 3, 16384, 512, 9)
     // if (x + 1) of above lists is a multiple of 3
 
-    val (multipleOfThree, notMultipleOfThree) = multipleOfThreeAndNotMultipleOfThree(powersOfTwo,powersOfTwoClean)
-
+    val (multipleOfThree, notMultipleOfThree) = (getListOfMultiplesOfThree() , getListOfNotMultiplesOfThree())
     println(s"${multipleOfThree}, ${notMultipleOfThree}")
-    // example - 8  is 2^3 so the power value is 3. If a number is not a power of 2 print a message and discard it
-    val powerValues = getPowerOfTwoValues(powersOfTwo)
 
+    // example - 8  is 2^3 so the power value is 3. If a number is not a power of 2 print a message and discard it
+    val powerValues = getPowerOfTwoValues()
     println(s" ${powerValues}")
-    val ascendingSortedPowerValues = sortTheList(powerValues)
+
+    val ascendingSortedPowerValues = sortTheList()
     println(s"${ascendingSortedPowerValues}")
+
     // take 2nd element to n-2nd element and sum the values and print it
     val slicedListSum : Int = sliceListSum(ascendingSortedPowerValues)
     println(s"${slicedListSum}")
 
-    def multipleOfThreeAndNotMultipleOfThree (powerOfTwo : List[Int], powerOfTwoClean : List[Int]): (List[Int],List[Int]) ={
-
-      val multipleOfThree : List[Int] = (powerOfTwo ++ powerOfTwoClean).filter{
-        x => (x+1)%3 == 0
+    def getListOfMultiplesOfThree ()={
+        (powersOfTwo ++ powersOfTwoClean).filter{ x =>
+        (x+1)%3 == 0
       }
-      val notMultipleOfThree : List[Int]= (powerOfTwo ++ powerOfTwoClean).filter{
-        x => (x+1)%3 != 0
-      }
-
-      (multipleOfThree,notMultipleOfThree)
     }
 
-    def getPowerOfTwoValues(powersOfTwo : List[Int]) : List[Int]={
+    def getListOfNotMultiplesOfThree () ={
+      (powersOfTwo ++ powersOfTwoClean).filter{ x =>
+        (x+1)%3 != 0
+      }
+    }
 
-      //      val powerOfTwo : List[Int]=powersOfTwo.filter{
-      //
-      //        x => if((x&(x-1))==0){
-      //          true
-      //        } else {
-      //          println(s" ${x} is not power of 2 ")
-      //          false
-      //        }
-      //
-      //      }
-      //      powerOfTwo
-
-
-
-      //      val valueOfPower : List[Int] =powersOfTwo.filter{ x =>
-      //        if((x&(x-1))==0){
-      //          true
-      //        } else {
-      //          println(s" ${x} is not power of 2 ")
-      //          false
-      //        }
-      //      }.map{ x =>
-      //        var count : Int = 0;
-      //        var valueOfElement : Int=x
-      //        while(valueOfElement>1){
-      //          count+=1
-      //          valueOfElement /=2
-      //        }
-      //        count
-      //      }
-      //      valueOfPower
-
-
+    def getPowerOfTwoValues() : List[Int]={
       powersOfTwo.filter{ x =>
         if((x&(x-1))==0){
           true
@@ -88,7 +57,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
       }
     }
 
-    def sortTheList( powerValues : List[Int]) : List[Int] = {
+    def sortTheList() : List[Int] = {
       powerValues.sorted
     }
 
@@ -110,39 +79,101 @@ import scala.concurrent.ExecutionContext.Implicits.global
     val jobs: Future[List[Job]] = Future.successful(List(Job("job1", "title1"), Job("job2", "title1"), Job("job3", "title3")))
     val clicks = Future.successful(List(ClicksStat("job2", 50),ClicksStat("job3", 20)))
     val applies = List(AppliesStat("job3", 150))
+
     // If stats are not present clicks/Applies should be None
-    val jobsEnrichedWithStats: Future[List[Job]] = for{
-      job <- jobs
-      click <- clicks
-    } yield{
-      val updatedJobListWithClick= job.map{
-        itr =>
-          (click.find(_.jobId==itr.jobId) , applies.find(_.jobId==itr.jobId)) match {
-            case (Some(v), Some(v1))=> itr.copy(clicks=Some{v.clicks}, applies=Some{v1.applies})
-            case (None , Some(v1))=> itr.copy(applies=Some{v1.applies})
-            case (Some(v) , None )=> itr.copy(clicks=Some{v.clicks})
-            case (None  , None )=> itr
-
-          }
-
-
-      }
-      updatedJobListWithClick
-    }// It should contain => Future(List(Job("job1", "title1"), Job("job2", "title1", Some(50), None), Job("job3", "title3", None, Some(150))))
-
-
+    val jobsEnrichedWithStats = jobsEnrichedWithStat()// It should contain => Future(List(Job("job1", "title1"), Job("job2", "title1", Some(50), None), Job("job3", "title3", None, Some(150))))
+    Await.result(jobsEnrichedWithStats , Duration.Inf)
     jobsEnrichedWithStats.foreach{
+      x => println(s"${x}")
+    }
+
+
+    val jobsWithClicksNotNone =  jobsWithClickNotNone()
+    Await.result(jobsWithClicksNotNone, Duration.Inf)
+    jobsWithClicksNotNone.foreach{
       x: List[Job] => println(s"${x}")
     }
 
-    Thread.sleep(5000)
-    val jobsWithClicksNotNone = ???
-    val jobsWithAppliesNotNone = ???
-    //  title -> List[Job]
-    val jobsGroupedByTitle = ???
-    // Should return Map(title -> (sumClicks, sumApplies)). if clicks/applies is None, set its value as 0
-    val statsPerTitle = ???
+    val jobsWithAppliesNotNone = jobsWithApplieNotNone()
+    Await.result(jobsWithAppliesNotNone, Duration.Inf)
+    jobsWithAppliesNotNone.foreach{
+      x => println(s"${x}")
+    }
 
+    //  title -> List[Job]
+    val jobsGroupedByTitle =  getListOfJobsGroupedByTitle()
+    Await.result(jobsGroupedByTitle , Duration.Inf)
+
+    jobsGroupedByTitle.foreach{ x=>
+      println(x)
+    }
+
+    // Should return Map(title -> (sumClicks, sumApplies)). if clicks/applies is None, set its value as 0
+    val statsPerTitle =  getSumOfClicksAndAplliesByTitle()
+    Await.result( statsPerTitle , Duration.Inf)
+
+    statsPerTitle.foreach{
+      x =>
+        println(x)
+    }
+
+
+    def jobsEnrichedWithStat() : Future[List[Job]] ={
+      for{
+        job <- this.jobs
+        click <- this.clicks
+      } yield{
+        val updatedJobListWithClick= job.map{
+          itr =>
+            (click.find(_.jobId==itr.jobId) , applies.find(_.jobId==itr.jobId)) match {
+              case (Some(v), Some(v1))=> itr.copy(clicks=Some{v.clicks}, applies=Some{v1.applies})
+              case (None , Some(v1))=> itr.copy(applies=Some{v1.applies})
+              case (Some(v) , None )=> itr.copy(clicks=Some{v.clicks})
+              case (None  , None )=> itr
+            }
+        }
+        updatedJobListWithClick
+      }
+    }
+
+    def jobsWithClickNotNone (): Future[List[Job]] ={
+      jobsEnrichedWithStats.map{ listOfJob =>
+
+        listOfJob.filter{ job =>
+          if(job.clicks==None) false else true
+        }
+      }
+    }
+
+    def jobsWithApplieNotNone (): Future[List[Job]] ={
+      jobsEnrichedWithStats.map{ listOfJob =>
+        listOfJob.filter{ job =>
+          if(job.applies==None) false else true;
+        }
+      }
+    }
+
+    def getListOfJobsGroupedByTitle(): Future[Map[String, List[Job]]] ={
+      jobsEnrichedWithStats.map{ listOfJob =>
+        listOfJob.groupBy(_.title)
+      }
+    }
+
+    def getSumOfClicksAndAplliesByTitle(): Future[Map[String, (Int, Int)]] ={
+      jobsGroupedByTitle.map{ mapOfJobByTitle =>
+
+        mapOfJobByTitle.map{ case (key ,value) =>
+            val sumOfClick = value.map{
+              x => x.clicks.getOrElse(0)
+            } .sum
+            val sumOfApplies = value.map{
+              x => x.applies.getOrElse(0)
+            } .sum
+
+            (key -> (sumOfClick,sumOfApplies))
+        }
+      }
+    }
 
 
   }
